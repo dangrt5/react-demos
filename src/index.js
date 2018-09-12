650ca71b285a72a6fe6f8199d7ba0a557e0026f9
dangrt5 <randy.dang05@gmail.com>
2018-09-12 16:43:00 -0700

    created dom elements with react react intro

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+const user = {
+  firstName: "Randy",
+  lastName: "Dang"
+};
+
+function fullName(user) {
+  return `${user.firstName} ${user.lastName}`;
+};
+
+const greeting = <h1>Hello {fullName(user)}</h1>;
+
+
+
 ReactDOM.render(
-    <App />,
+    greeting,
     document.getElementById('root')
 );
