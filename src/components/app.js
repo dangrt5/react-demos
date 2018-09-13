8249efd524a92d0c17944e2ef82bc3f424ac5534
dangrt5 <randy.dang05@gmail.com>
2018-09-12 18:35:23 -0700

    functional components challenge complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import Greeting from "./greeting";
 
 const App = () => (
     <div>
         <div className="app">
             <img src={logo} className="logo rotate"/>
+            <Greeting name="Randy"/>
             <h1>Welcome to React</h1>
         </div>
     </div>
---
+++ src/components/greeting.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Greeting(props) {
+  return <h1>Hello {props.name}, welcome to the challenge!</h1>
+
+}
+
+export default Greeting;
