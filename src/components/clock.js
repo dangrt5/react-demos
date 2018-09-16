9e73ec74b1bd7ad094fea67a4a76155cd314f530
dangrt5 <randy.dang05@gmail.com>
2018-09-15 18:17:05 -0700

    time class component

---
+++ src/components/clock.js
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+import Time from "./time"
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: new Date()
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => this.tick(), 1000)
+  }
+
+  tick() {
+    this.setState({
+      time: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <h1>The time is currently: <Time time={this.state.time} /> </h1>
+    )
+  }
+
+};
+
+export default Clock;
