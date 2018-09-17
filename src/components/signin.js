35a0bae41323b3f706cdee08e69f48b04c7b20d2
dangrt5 <randy.dang05@gmail.com>
2018-09-17 15:05:35 -0700

    Input state challenge

---
+++ src/components/signin.js
@@ -0,0 +1,42 @@
+import React, {Component} from "react";
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault;
+  }
+
+
+  render() {
+    return (
+      <form>
+        <div>
+          <label>Email</label>
+          <input name="email" type="email" autoComplete="off" onChange={this.handleChange} ></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" onChange={this.handleChange}></input>
+        </div>
+        <button type="submit">Submit Form</button>
+      </form>
+    )
+  }
+}
+
+export default SignIn;
